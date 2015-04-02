ce1bd2944c316b02bd6b25fbbd08ba8b43a07ab6
Logan Koester <logan@logankoester.com>
2015-04-02 05:41:08 -0400

    Adds consul_template configuration values to template from attributes

---
+++ attributes/default.rb
@@ -0,0 +1,13 @@
+default['consul_template']['config'] = {
+  auth: {
+  }, 
+  consul: '127.0.0.1:8500',
+  'max-stale': 0,
+  'ssl': false,
+  'ssl-verify': true,
+  'syslog': false,
+  'syslog-facility': 'LOCAL0',
+  'token': nil,
+  'wait': nil,
+  'retry': '5s'
+}
