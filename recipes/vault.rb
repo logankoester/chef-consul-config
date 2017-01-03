71fa01c17df55d5372465c196a5fc4c18489411a
Logan Koester <logan@logankoester.com>
2017-01-03 21:09:07 +0000

    Trust the CA certificate system-wide

--- recipes/vault.rb
+++ recipes/vault.rb
@@ -1,8 +1,14 @@
 # Generate and sign a certificate for Vault with the CA cert and key
-# stored in chef-vault.
+# stored in chef-vault, then add the CA cert to the system trust store.
 # 
 # See https://supermarket.chef.io/cookbooks/chef_vault_pki
 # See https://www.vaultproject.io/docs/config/index.html
+
+# Install and load chef-vault
+chef_gem 'chef-vault' do
+  compile_time true if respond_to?(:compile_time)
+end
+require 'chef-vault'
 
 tls_cert_dir = Pathname.new(node['hashicorp-vault']['config']['tls_cert_file']).dirname.to_s
 
@@ -23,4 +29,11 @@ chef_vault_pki node['consul-config']['vault']['tls']['cn'] do
   public_mode 0644
   private_mode 0600
   bundle_ca node['consul-config']['vault']['tls']['bundle_ca']
+end
+
+# Add the CA cert to the system trust store
+ca = ChefVault::Item.load(node['chef_vault_pki']['data_bag'], node['chef_vault_pki']['ca'])
+trusted_certificate node['chef_vault_pki']['ca'] do
+  action :create
+  content ca['cert']
 end
